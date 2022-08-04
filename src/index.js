e0879dfc774b2d81c2028f108c86418a6841fff0
kirat692157 <45176400+kirat692157@users.noreply.github.com>
2022-08-05 01:44:58 +0530

    Test

--- src/index.js
+++ src/index.js
@@ -32,7 +32,7 @@ class App extends Component{
     render(){
         const videoSearch = _.debounce((term)=>{this.videoSearch(term)},300);
         return ( 
-            <div>
+            <div class = "main">
                 <SearchBar onSearchTermChange={videoSearch} />
                 <VideoDetail video = {this.state.selectedVideo}/>
                 <VideoList 
